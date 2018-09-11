af4d2fdf36985d563221a123db5786ea703ccef0
Celso Cercado Jr. <38006556+Celso-Cercado-Jr@users.noreply.github.com>
2018-09-11 12:51:27 -0400

    1

--- ide-7.0-workspace/cst8244.ws/my_first_project_lab01/src/my_first_project_lab01.c
+++ ide-7.0-workspace/cst8244.ws/my_first_project_lab01/src/my_first_project_lab01.c
@@ -3,5 +3,11 @@
 
 int main(void) {
 	puts("Hello World!!!"); /* prints Hello World!!! */
+	puts("@author Celso Cercado Jr. (cerc0004@algonquinlive.com)");
+	puts("I ride a bus sometimes.!!! xD");
+
 	return EXIT_SUCCESS;
+
+
+
 }
