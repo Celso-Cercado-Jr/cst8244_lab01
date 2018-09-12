34d0a6103bcc6296786fb733b53c80e31100cc89
Celso Cercado Jr. <38006556+Celso-Cercado-Jr@users.noreply.github.com>
2018-09-12 15:11:23 -0400

    lab01 :: Finished

--- my_first_project/src/my_first_project.c
+++ my_first_project/src/my_first_project.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("Hello World!!!"); /* prints Hello World!!! */
+	puts("Hello World from QNX Neutrino RTOS!!!"); /* prints Hello World!!! */
+	puts("@author Celso Cercado Jr. (cerc0004@algonquinlive.com)");
+	puts("I like to play basketball");
 	return EXIT_SUCCESS;
 }
