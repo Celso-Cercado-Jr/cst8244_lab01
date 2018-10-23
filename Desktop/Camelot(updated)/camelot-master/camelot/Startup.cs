a49e7dcbeb599005b1f6af5bc9b51ae11d16740b
Celso Cercado Jr. <38006556+Celso-Cercado-Jr@users.noreply.github.com>
2018-10-23 09:58:13 -0400

    test

---
+++ Desktop/Camelot(updated)/camelot-master/camelot/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+
+[assembly: OwinStartupAttribute(typeof(Camelot.Startup))]
+namespace Camelot
+{
+    public partial class Startup
+    {
+
+
+        public void Configuration(IAppBuilder app)
+        {
+
+            ConfigureAuth(app);// default
+
+        }
+
+
+
+    }
+}
